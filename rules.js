92b3835747a307be9b6df63d516c5f4863f575d3
PkuCuipy <pkucuipy@icloud.com>
2025-12-11 23:08:58 -0800

    separate files; add space on start/end of url; refined UI

---
+++ rules.js
@@ -0,0 +1,8 @@
+// URL cleaning rules
+// Format: { "domain": ["param1", "param2", ...] }
+// Empty array means remove all parameters
+const URL_RULES = {
+  "xiaohongshu.com": ["xsec_token"],
+  "zhihu.com": [],
+  "bilibili.com": ["p", "t"]
+};
